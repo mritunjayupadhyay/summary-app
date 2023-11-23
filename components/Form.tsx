2fa1b9ffb5d8c59f80c2f3d4bf11662d8e6d899e
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2023-11-24 04:57:15 +0700

    fix for eslint

--- components/Form.tsx
+++ components/Form.tsx
@@ -51,7 +51,7 @@ function Form() {
           <img src={"/loader.svg"} alt='loader' className='w-20 h-20 object-contain' />
         ) : error ? (
           <p className='font-inter font-bold text-black text-center'>
-            Well, that wasn't supposed to happen...
+            {`Well, that wasn't supposed to happen...`}
             <br />
             <span className='font-satoshi font-normal text-gray-700'>
               {'error'}
